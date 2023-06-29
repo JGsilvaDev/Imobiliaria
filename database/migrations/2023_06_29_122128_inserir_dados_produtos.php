25dc2962ad4a65647662a556a807508961f8af50
João Gabriel <jgoliveira445@gmail.com>
2023-06-29 09:28:16 -0300

    migrations para trazer os dados que tem que estarem pre cadastrados

---
+++ database/migrations/2023_06_29_122128_inserir_dados_produtos.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        DB::table('produtos')->insert([
+            ['id' => 1, 'descricao' => 'Terreno'],
+            ['id' => 2, 'descricao' => 'Casa'],
+            ['id' => 3, 'descricao' => 'Apartamento'],
+
+            // Adicione mais registros, se necessário
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        DB::table('produtos')->truncate();
+    }
+};

b8b23f0e69effce557415abf0a8370aeee4f7b44
RodrigoTheDev <onovodash@outlook.com.br>
2023-07-17 15:31:03 -0300

    Mudanças drásticas na tela de contato

--- resources/views/contato.blade.php
+++ resources/views/contato.blade.php
@@ -5,12 +5,15 @@
 @section('content')
 
     <link rel="stylesheet" href="{{ asset('css/contato.css') }}">
-    <div id="contato-container">
-        <h1>Contato</h1>
-        <p id="contato-email">imobiliaria@email.com</p>
-        <p id="contato-instagram">@imobiliaria.2020</p>
-        <p id="contato-telefone">3101-9572</p>
-        <p id="contato-whatsapp">(12) 99746-5262</p>
+    <div class="contato-container">
+        <h1 class="contato-title">Fale conosco!</h1>
+        <form action="" id="contato-content" autocomplete="off">
+            <input type="text" name="" id="" class="contato-input" placeholder="nome">
+            <input type="text" name="" id="" class="contato-input" placeholder="telefone">
+            <input type="email" name="" id="" class="contato-input" placeholder="email">
+            <textarea name="" id="" cols="30" rows="10" class="contato-input" style="resize:none" placeholder="mensagem"></textarea>
+            <button class="contato-enviar">enviar</button>
+        </form>
     </div>
 
     {{-- <p id="voltar-btn"><a href="#">Voltar para página inicial</a></p> --}}

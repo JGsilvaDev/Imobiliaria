a7a55593886df62f8ce8156ec6351571f67f11ff
RodrigoTheDev <onovodash@outlook.com.br>
2023-07-31 19:39:59 -0300

    Responsividade do carrossel corrigida

--- public/js/caroselDestaque.js
+++ public/js/caroselDestaque.js
@@ -3,6 +3,9 @@ document.addEventListener('DOMContentLoaded', function() {
     $(document).ready(function () {
 
         let count = document.getElementById('count').value;
+        if(!widthLowerThan(610)) {
+            count = 1;
+        }
 
         if(count > 3){
             $("#destaques-content").slick({
@@ -17,7 +20,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 clone: false,
             });
 
-        }else{
+        }else if(count == 2){
             $("#destaques-content").slick({
                 dots: true,
                 slidesToShow: 2,
@@ -27,8 +30,31 @@ document.addEventListener('DOMContentLoaded', function() {
                 clone: false,
                 swipe :false,
             });
+        }else if(count == 1) {
+            $("#destaques-content").slick({
+                dots: true,
+                infinite: true,
+                speed: 500,
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                autoplay: true,
+                autoplaySpeed: 5000,
+                arrows: false,
+                clone: false,
+            });
         }
 
     });
 
 });
+
+
+function checkRes() {
+    console.log('altura: '+window.screen.availHeight)
+    console.log('largura: '+window.screen.availWidth)
+}
+
+function widthLowerThan(largura) {
+    if (window.screen.availWidth > largura) { return true }
+    else return false
+} 

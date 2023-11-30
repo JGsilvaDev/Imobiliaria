7ae7565517d8ac5016c9a0f6272ad1c168931cfb
RodrigoTheDev <82519551+RodrigoTheDev@users.noreply.github.com>
2023-11-30 09:04:59 -0300

    fix: texto da política de privacidade

--- resources/views/info/politica_privacidade.blade.php
+++ resources/views/info/politica_privacidade.blade.php
@@ -15,7 +15,36 @@
                     <img class="imgSobre" src="{{ asset('img/logo.png') }}" alt="">
                 </div>
                 <div id="sobre-texto">
-                    <p>[TEXTO POLÍTICA DE PRIVACIDADE]<br><br>
+                    <p>Política de Privacidade da Imobiliária Eunice Socolowski</p>
+                    <p>A sua privacidade é uma prioridade para nós na Imobiliária Eunice Socolowski. Esta Política de Privacidade explica como coletamos, usamos, compartilhamos e protegemos as informações pessoais que você nos fornece ao usar nossos serviços.</p>
+                    
+                    <p>Informações Coletadas</p>
+                    <p>Coletamos informações pessoais, como nome, endereço de e-mail, número de telefone e informações relacionadas à propriedade, quando você se registra em nossos serviços ou interage conosco.</p>
+
+                    <p>Uso das Informações</p>
+                    <p>As informações pessoais coletadas são usadas para fornecer e melhorar nossos serviços, personalizar sua experiência, facilitar transações imobiliárias, responder a suas solicitações e enviar comunicações relevantes.</p>
+
+                    <p>Compartilhamento de Informações</p>
+                    <p>Não vendemos, alugamos ou compartilhamos suas informações pessoais com terceiros sem o seu consentimento, exceto quando necessário para cumprir obrigações legais ou facilitar transações imobiliárias.</p>
+
+                    <p>Segurança das Informações</p>
+                    <p>Implementamos medidas de segurança para proteger suas informações pessoais contra acesso não autorizado, alteração, divulgação ou destruição.</p>
+
+                    <p>Cookies e Tecnologias Semelhantes</p>
+                    <p>Utilizamos cookies e tecnologias semelhantes para melhorar a funcionalidade do nosso site e coletar informações sobre o uso do mesmo. Você pode gerenciar suas preferências de cookies nas configurações do seu navegador.</p>
+
+                    <p>Acesso e Controle das Informações</p>
+                    <p>Você tem o direito de acessar, corrigir ou excluir suas informações pessoais a qualquer momento. Para exercer esses direitos ou se tiver dúvidas sobre esta Política de Privacidade, entre em contato conosco através da <a href="/contato">página de contato</a>.</p>
+
+                    <p>Alterações na Política de Privacidade</p>
+                    <p>Esta Política de Privacidade pode ser atualizada periodicamente. Recomendamos revisar esta página regularmente para estar ciente de quaisquer alterações.</p>
+
+                    <p>Ao continuar a utilizar os serviços da Imobiliária Eunice Socolowski, você concorda com esta Política de Privacidade.</p>
+
+                    <p>Obrigado por confiar em nós para suas necessidades imobiliárias.</p>
+
+                    <p>Atenciosamente, A Equipe da Imobiliária Eunice Socolowski</p>
+
                 </div>
             </div>
         </section>

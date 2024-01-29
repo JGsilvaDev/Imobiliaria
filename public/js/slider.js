c98600ad03c3c50f37ab2bf8b9beb111c5298083
RodrigoTheDev <rocapucho123123@gmail.com>
2024-01-29 11:28:20 -0300

    fix: problema nos sliders

--- public/js/slider.js
+++ public/js/slider.js
@@ -1,8 +1,24 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-    const sliders = [document.getElementById('sliderBanheiro'), document.getElementById('sliderQuartos'), document.getElementById('sliderVagas'), document.getElementById('sliderVagasNaoCoberto'), document.getElementById('sliderSacadas'), document.getElementById('sliderSuite'), document.getElementById('sliderBanheirosAp'), document.getElementById('sliderQuartosAp'), document.getElementById('sliderVagasAp'), document.getElementById('sliderVagasNaoCobertoAp'), document.getElementById('sliderSuiteAp')]
+    const sliders = [
+        document.getElementById('sliderBanheiro'),
+        document.getElementById('sliderSuite'),
+        document.getElementById('sliderQuartos'),
+        document.getElementById('sliderVagas'),
+        document.getElementById('sliderVagasNaoCoberto'),
+        document.getElementById('sliderSalas'),
+        document.getElementById('sliderDorms')
+    ]
 
-    const slidersValue = [document.getElementById('sliderValueBanheiro'), document.getElementById('sliderValueQuartos'), document.getElementById('sliderValueVagas'), document.getElementById('sliderValueVagasNaoCoberto'), document.getElementById('sliderValueSacadas'), document.getElementById('sliderValueSuite'), document.getElementById('sliderValueBanheirosAp'), document.getElementById('sliderValueQuartosAp'), document.getElementById('sliderValueVagasAp'), document.getElementById('sliderValueVagasNaoCobertoAp'), document.getElementById('sliderValueSuiteAp')]
+    const slidersValue = [
+        document.getElementById('sliderValueBanheiro'),
+        document.getElementById('sliderValueSuite'),
+        document.getElementById('sliderValueQuartos'),
+        document.getElementById('sliderValueVagas'),
+        document.getElementById('sliderValueVagasNaoCoberto'),
+        document.getElementById('sliderValueSalas'),
+        document.getElementById('sliderValueDorms')
+    ]
 
 
     sliders.forEach(function(elem, index) {
